5b65cf0e9a37edc8f95ecda604f537a24663478c
lince <lincechacko99@gmail.com>
2026-01-20 17:49:43 +0530

    started the dummy driver project which act like real hardwarew

---
+++ dummy_driver_usr_kernel/kernal_driver.c
@@ -0,0 +1,11 @@
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/init.h>
+#include<linux/fs.h>
+#include<linux/kdev_t.h>
+#include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/err.h>
+#include<linux/slab.h>
+#include<linux/uaccess.h>
+
